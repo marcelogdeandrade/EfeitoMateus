9876e53b1fb390563e369ab4c724226e2fd009d7
marcelogdeandrade@gmail.com <marcelogdeandrade@gmail.com>
2017-11-13 21:28:27 -0200

    trying to fix window error

--- src/index.js
+++ src/index.js
@@ -5,6 +5,32 @@ let currentApp = app;
 
 server.listen(process.env.PORT || 3000);
 
+const fs = require('fs');
+
+const visibilityFile = 'node_modules/semantic-ui-react/dist/commonjs/behaviors/Visibility/Visibility.js';
+const stickyFile = 'node_modules/semantic-ui-react/dist/commonjs/modules/Sticky/Sticky.js';
+
+fs.readFile(visibilityFile, 'utf8', function (err, data) {
+  if (err) {
+    return console.error(err);
+  }
+  const result = data.replace('context: window', 'context: typeof window === \'object\' && window !== null && window.self === window ? window : {}');
+
+  fs.writeFile(visibilityFile, result, 'utf8', function (err) {
+    if (err) return console.error(err);
+  });
+});
+
+fs.readFile(stickyFile, 'utf8', function (err, data) {
+  if (err) {
+    return console.error(err);
+  }
+  const result = data.replace('scrollContext: window', 'scrollContext: typeof window === \'object\' && window !== null && window.self === window ? window : {}');
+
+  fs.writeFile(stickyFile, result, 'utf8', function (err) {
+    if (err) return console.error(err);
+  });
+});
 
 if (module.hot) {
   console.log('✅  Server-side HMR Enabled!');
