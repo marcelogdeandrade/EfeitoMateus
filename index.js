747553e275c866d42ba639ef5ad06fb85852c85c
marcelogdeandrade@gmail.com <marcelogdeandrade@gmail.com>
2017-11-13 09:12:47 -0200

    adds first view and mongoose

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+
+const mongoUrl = 'mongodb://MarceloPedroRedes:marcelopedro1@ds255715.mlab.com:55715/efeito-mateus'
+mongoose.connect(mongoUrl, {
+  useMongoClient: true
+});
+var db = mongoose.connection;
+
+db.createCollection("Videos", function(err, res) {
+  if (err) console.log(err);
+  console.log(("Collection created!"))
+  db.close();
+});
 
 // Constants
 const PORT = 8081;
@@ -6,8 +20,16 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
+
+app.set('views', './views');
+app.set('view engine', 'jade');
+
+
 app.get('/', (req, res) => {
-  res.send('Hello world\n');
+  res.render('home', {
+    title: 'Welcome',
+    user: { 'fullname' : 'Marcelo'}
+  });
 });
 
 app.listen(PORT);
